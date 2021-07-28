ce082f9eee6f7126a4f30bf73a4c8486d8ad91c1
nustar <tlstjd436@naver.com>
2021-07-28 20:06:55 +0900

    608 : 문자열2 - 자가진단7

---
+++ JUNGOL/src/Main_608.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main_608 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		if(line.contains("c")) {
+			System.out.print("Yes");
+		}else {
+			System.out.print("No");
+		}
+		System.out.print(" ");
+		if(line.contains("ab")) {
+			System.out.print("Yes");
+		}else {
+			System.out.print("No");
+		}
+	}
+}
