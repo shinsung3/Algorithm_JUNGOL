246eefd7ff6c267d06ce516a8941679da7d983a4
nustar <tlstjd436@naver.com>
2021-08-03 14:56:29 +0900

    Level1 약수의 개수와 덧셈

---
+++ PROGRAMMERS/src/Level1/Main_약수의개수와덧셈.java
@@ -0,0 +1,28 @@
+package Level1;
+
+public class Main_¾à¼öÀÇ°³¼ö¿Íµ¡¼À {
+	public static void main(String[] args) {
+		int left = 13;
+		int right = 17;
+		int answer = solution(left, right);
+		System.out.println(answer);
+	}
+
+	public static int solution(int left, int right) {
+		int answer = 0;
+		for (int i = left; i <= right; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					cnt++;
+				}
+			}
+			if (cnt % 2 == 0) {
+				answer += i;
+			} else {
+				answer -= i;
+			}
+		}
+		return answer;
+	}
+}
