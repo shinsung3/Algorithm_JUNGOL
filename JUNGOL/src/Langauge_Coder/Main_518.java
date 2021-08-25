93ff28df5317f53ced3d23ea8f28c5f600d4fa2f
nustar <tlstjd436@naver.com>
2021-08-25 19:52:53 +0900

    518 : 연산자 - 자가진단1

---
+++ JUNGOL/src/Langauge_Coder/Main_518.java
@@ -0,0 +1,15 @@
+package Langauge_Coder;
+
+import java.util.Scanner;
+
+public class Main_518 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println("sum : "+(a+b+c));
+		System.out.println("avg : "+((a+b+c)/3));
+	}
+}
